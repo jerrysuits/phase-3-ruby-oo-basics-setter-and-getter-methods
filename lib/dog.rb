90d111e06d9836922e04485b7942bf970428dc94
Gerald <adudagerald@gmail.com>
2023-05-22 15:24:35 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+     def name=(name)
+          @name = name
+        end
+end
 
+class Dog
+     def name
+       @name
+     end
+   end
+   
+class Dog
+     def breed=(breed)
+          @breed = breed
+     end
+end
+
+class Dog
+     def breed
+       @breed
+     end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
-class Person
 
-end
+class Person
+          def name=(name)
+            @name = name
+          end
+        end
+        
+
+class Person
+     def name
+       @name
+     end
+   end
+   
+
+class Person
+     def job=(job)
+       @job = job
+     end
+   end
+   
+
+class Person
+     def job
+       @job
+     end
+   end
+   
